05aab6177b1191ecd0358d55460ec59b8107206e
Christine <yutingcxiang@gmail.com>
2018-08-29 19:08:39 -0400

    Adjusted columns and layout for study_sessions

--- db/migrate/20180829104631_create_study_sessions.rb
+++ db/migrate/20180829104631_create_study_sessions.rb
@@ -1,9 +1,8 @@
 class CreateStudySessions < ActiveRecord::Migration
   def change
-    create_table study_sessions do |t|
+    create_table :study_sessions do |t|
       t.date :date
       t.string :section_name
-      t.string :item
       t.decimal :hours
       t.integer :user_id
     end
