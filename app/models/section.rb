57b3a35ba4494e00b9a46d06947a543491c32130
Christine <yutingcxiang@gmail.com>
2018-08-29 06:55:04 -0400

    Added validation and helper methods for models

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,3 +1,4 @@
 class Section < ActiveRecord::Base
   belongs_to :student
+  validates_presence_of :name, :item
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,15 @@
 class Student < ActiveRecord::Base
   has_many :sections
   has_secure_password
+  validates_presence_of :name, :username, :password, :email
+
+  def slug
+    self.username.gsub(" ","-").downcase
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find do |user|
+    user.slug == slug
+  end
+end
 end
