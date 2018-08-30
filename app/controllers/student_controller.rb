cb803cd70dc6ce704bd607010fc61284ae101f1b
Christine <yutingcxiang@gmail.com>
2018-08-30 01:43:02 -0400

    Add routes for signup/login/show for StudentsController

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,3 +1,28 @@
 class StudentController < ApplicationController
+  get '/students/:slug' do
+    if logged_in?
+      @student = Student.find_by_slug(params[:slug])
+      erb :'students/show'
+    else
+      redirect '/'
+    end
+  end
+
+  get '/login' do
+    if logged_in?
+      redirect :'/study_sessions/study_session'
+    else
+      erb :'students/login'
+    end
+  end
+
+  get '/signup' do
+    if logged_in?
+      redirect :'/study_sessions/study_session'
+    else
+      erb :'students/signup'
+    end
+  end
+
 
 end
