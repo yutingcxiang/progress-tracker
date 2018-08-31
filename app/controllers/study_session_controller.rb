3ab33861324967ad8e20f7975a4332f8694f4914
Christine <yutingcxiang@gmail.com>
2018-08-30 22:47:07 -0400

    Add rack_flash messages

--- app/controllers/study_session_controller.rb
+++ app/controllers/study_session_controller.rb
@@ -19,8 +19,8 @@ class StudySessionController < ApplicationController
 
   post '/study_sessions' do
     if params[:date].empty? || params[:hours].empty? || params[:summary].empty?
-      redirect '/study_sessions/new_study_session'
       flash[:message] = "Error. Please fill out all fields and try again."
+      redirect '/study_sessions/new_study_session'
     else
       @study_session = StudySession.new(date: params[:date], hours: params[:hours], summary: params[:summary])
       @study_session.student = current_student
@@ -40,8 +40,8 @@ class StudySessionController < ApplicationController
 
   get '/study_sessions/:id/edit' do
     if logged_in?
-      @study_session = StudySession.find_by(id: params[:id])
       erb :'study_sessions/edit_study_session'
+      @study_session = StudySession.find_by(id: params[:id])
     else
       redirect '/login'
     end
@@ -49,8 +49,8 @@ class StudySessionController < ApplicationController
 
   patch '/study_sessions/:id' do
     if params[:date].empty? || params[:hours].empty? || params[:summary].empty?
-      redirect "/study_sessions/#{@study_session.id}/edit"
       flash[:message] = "Error. Please fill out all fields and try again."
+      redirect "/study_sessions/#{@study_session.id}/edit"
     else
       if @study_session && @study_session.student.id == curent_student.id
         @study_session = StudySession.find_by(id: params[:id])
@@ -70,8 +70,8 @@ class StudySessionController < ApplicationController
       if @study_session && @study_session.student == current_student
         @study_session.delete
       end
-      redirect '/study_sessions'
       flash[:message] = "Success. Study session has been deleted."
+      redirect '/study_sessions'
     else
       redirect '/login'
     end
