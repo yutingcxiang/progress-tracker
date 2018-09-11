c8aae8b2eef7899c661c24c541e99510b2835363
Christine <yutingcxiang@gmail.com>
2018-09-10 20:02:03 -0400

    Update /study_session route to /my_study_sessions

--- app/controllers/study_session_controller.rb
+++ app/controllers/study_session_controller.rb
@@ -8,7 +8,7 @@ class StudySessionController < ApplicationController
     end
   end
 
-  get '/study_sessions/new' do
+  get '/my_study_sessions/new' do
     if logged_in?
       erb :'study_sessions/new_study_session'
     else
@@ -16,20 +16,20 @@ class StudySessionController < ApplicationController
     end
   end
 
-  post '/study_sessions' do
+  post '/my_study_sessions' do
     if params[:date].empty? || params[:hours].empty? || params[:summary].empty?
       flash[:message] = "Error. Please fill out all fields and try again."
-      redirect '/study_sessions/new_study_session'
+      redirect '/my_study_sessions/new'
     else
       @study_session = StudySession.new(date: params[:date], hours: params[:hours], summary: params[:summary])
       @study_session.student = current_student
       @study_session.save
       flash[:message] = "Success. Study session created."
-      redirect "/study_sessions/#{@study_session.id}"
+      redirect "/my_study_sessions/#{@study_session.id}"
     end
   end
 
-  get '/study_sessions/:id' do
+  get '/my_study_sessions/:id' do
     if logged_in?
       @study_session = StudySession.find_by(id: params[:id])
       erb :'study_sessions/show_study_session'
@@ -38,7 +38,7 @@ class StudySessionController < ApplicationController
     end
   end
 
-  get '/study_sessions/:id/edit' do
+  get '/my_study_sessions/:id/edit' do
     if logged_in?
       @study_session = StudySession.find_by(id: params[:id])
       if @study_session && @study_session.student.id == current_student.id
@@ -49,24 +49,24 @@ class StudySessionController < ApplicationController
     end
   end
 
-  patch '/study_sessions/:id' do
+  patch '/my_study_sessions/:id' do
     if params[:date].empty? || params[:hours].empty? || params[:summary].empty?
       flash[:message] = "Error. Please fill out all fields and try again."
-      redirect "/study_sessions/#{@study_session.id}/edit"
+      redirect "/my_study_sessions/#{@study_session.id}/edit"
     else
       if params[:id] == current_study_session.id
         @study_session = StudySession.find_by(id: params[:id])
         @study_session.update(date: params[:date], hours: params[:hours], summary: params[:summary])
         @study_session.student = current_student
         @study_session.save
-        redirect "/study_sessions/#{@study_session.id}"
+        redirect "/my_study_sessions/#{@study_session.id}"
       else
         redirect '/my_study_sessions'
       end
     end
   end
 
-  delete '/study_sessions/:id' do
+  delete '/my_study_sessions/:id' do
     @study_session = StudySession.find_by(id: params[:id])
     if logged_in?
       if @study_session && @study_session.student == current_student
